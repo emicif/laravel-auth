7ef9c9148513d32d5e85d98027fb2207d13f1041
emicif <96519927+emicif@users.noreply.github.com>
2022-05-30 15:16:47 +0200

    home controller/create home.blade login

--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,22 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+/*
 Route::get('/', function () {
     return view('welcome');
 });
-
+*/
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::middleware('auth')
+    ->namespace('Admin')
+    ->name('admin.')
+    ->prefix('admin')
+    ->group(function () {
+        Route::get('/', 'HomeController@index')
+        ->name('home');
+});
+
